bff86d424fc788de7cc2e7a3736aad2090e73dce
Oleksandr <online4ik3773@gmail.com>
2022-02-07 18:37:08 +0200

    adaptive prod list

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -6,12 +6,14 @@ function creatProdList(json){
     let html = '';
     for(let i=0;i<json.length;i++){
         html += `
-        <li class="item"><a href="#">
+        <li class="item"><a href="#" class="item_link">
             <div class="image"><img src="${json[i].image}" alt="product image"></div>
-            <p class="text">${json[i].description}</p>
-            <div class="rait_wrap">
-                <div class="${json[i].rait}"></div>
-                <span class="qty">${json[i].qtyRev}</span>
+            <div class="text_wrap">
+                <p class="text">${json[i].description}</p>
+                <div class="rait_wrap">
+                    <div class="${json[i].rait}"></div>
+                    <span class="qty">${json[i].qtyRev}</span>
+                </div>
             </div>
         </a></li>`;
     }
